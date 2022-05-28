a534e2a37dd6d54d9a50c97b9065fb6da8057348
Chris Faust <hello@chrisfaust.dev>
2022-05-28 13:56:50 -0700

    Added new projects to the page. changed css styling to put margin between the projects.  Also fixed some issues when moving to mobile first.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// TODO: Refactor code to include js using angular
+// TODO: for portfolio items, use JS and for loop to render each card as a template
+// TODO: header create a for loop that will create unordered list that creates 3 li that include a tags
+// TODO: About me section create a for loop that will create the about me section
+
